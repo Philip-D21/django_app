baff4eafa39fde35a27d09705b45a22907ebf6ba
Philip-D21 <philiplekan88@gmail.com>
2024-03-22 22:41:50 +0100

    ...

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -9,16 +9,17 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import dj_database_url
+import os
+
 
 from pathlib import Path
-
 
 #envirinment variables
 import environ
 env = environ.Env()
 
 environ.Env.read_env()
-
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -34,7 +35,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-app-qxvh.onrender.com']
+ALLOWED_HOSTS = ['django-app-qxvh.onrender.com',  '127.0.0.1']
 
 
 # Application definition
@@ -92,15 +93,20 @@ DATABASES = {
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
 
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': env("DB_NAME"),
+    #     'USER': env("DB_USER"),
+    #     'PASSWORD': env("DB_PASSWORD"),
+    #     'HOST': env("DB_HOST"),
+    #     'PORT': env("DB_PORT"),
         
-    }
+    # }
+
+    
+ 
+	"default": dj_database_url.parse(os.environ.get("DB_URL"))
+
 }
 
 
